14eb130d6b7a01f679e95a83f84d646e9ba19bb8
daniel <dan.barbosa0702@gmail.com>
2024-06-25 20:18:57 -0300

    teste

---
+++ teste1.py
@@ -0,0 +1,5 @@
+nome = (input('qual é o seu nome? '))
+idade =  int(input("qual é a sua idade? "))
+altura = input('qual é a sua altura ? ')
+
+print('ola' ,nome , ',seja bem vindo, você tem' ,idade , 'anos, e mede' , altura , )
