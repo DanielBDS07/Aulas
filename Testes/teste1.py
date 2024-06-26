51c08a47aa71431fce9bba486edc59ed20dcc19d
daniel <dan.barbosa0702@gmail.com>
2024-06-26 19:14:52 -0300

    Treinando

---
+++ Testes/teste1.py
@@ -0,0 +1,25 @@
+acessar = (input('quer entrar na calculadora [s/n]: '))
+while acessar == 's':
+
+    n1 = float(input("Primeiro digito: ")) 
+    O1 = input("Operadores Aritméticos (+,-,*,/): ") 
+    n2 = float(input("Segundo digito: ")) 
+
+    match O1:
+        case "+":
+            print(n1 + n2)
+        case "-":
+            print(n1 - n2)
+        case"*":
+            print(n1 * n2)
+        case "/":
+            if n2 == 0:
+                print("Não se pode dividir por 0")
+            else:
+                print(n1 / n2)
+
+        case _:
+            print("Operador incorreto, por favor, coloque um operador valido")
+    acessar = (input('quer continuar [s/n]: '))
+else: 
+    print('muito obrigado por acessar')
